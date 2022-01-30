e6eb19d0fb526ee33ceb242e9bc6a24aa6538d28
AndrijaSunko <andrijasunko@gmail.com>
2022-01-31 00:26:49 +0100

    seed data

---
+++ Vehicles/Models/SeedData.cs
@@ -0,0 +1,101 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Vehicles.Data;
+using Vehicles.Models;
+using System;
+using System.Linq;
+
+
+namespace Vehicles.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.VehicleMake.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.VehicleMake.AddRange(
+
+                    new VehicleMake
+                    {
+                        Name = "BMW",
+                        Abrv = "BMW"
+                    },
+
+                    new VehicleMake
+                    {
+                        Name = "Volkswagen",
+                        Abrv = "VW"
+                    },
+
+                    new VehicleMake
+                    {
+                        Name = "Ford",
+                        Abrv = "Ford"
+                    },
+                    new VehicleMake
+                    {
+                        Name = "Citroen",
+                        Abrv = "Cit"
+                    },
+                    new VehicleMake
+                    {
+                        Name = "Rolls Royce",
+                        Abrv = "RR"
+                    }
+
+
+
+
+                );
+                context.SaveChanges();
+
+                context.VehicleModel.AddRange(
+
+                    new VehicleModel
+                    {
+                        MakeId = 1,
+                        Name = "BMW",
+                        Abrv = "x5",
+                    },
+                    new VehicleModel
+                    {
+                        MakeId = 2,
+                        Name = "Volkswagen",
+                        Abrv = "Golf",
+                    },
+                    new VehicleModel
+                    {
+                        MakeId = 3,
+                        Name = "Ford",
+                        Abrv = "Focus",
+                    },
+                    new VehicleModel
+                    {
+                        MakeId = 4,
+                        Name = "Citroen",
+                        Abrv = "C5",
+                    },
+                    new VehicleModel
+                    {
+                        MakeId = 5,
+                        Name = "Rolls Royce",
+                        Abrv = "PE",
+                    }
+
+
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
