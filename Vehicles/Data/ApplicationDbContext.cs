d2ca6ba7c74ff9fb26b95dc638bad6f8ea4968ce
AndrijaSunko <andrijasunko@gmail.com>
2022-01-31 15:08:02 +0100

    Admin menu layout

--- Vehicles/Data/ApplicationDbContext.cs
+++ Vehicles/Data/ApplicationDbContext.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Vehicles.Models;
 
 namespace Vehicles.Data
 {
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(250)]
+        public string FirstName { get; set; }
+        [StringLength(250)]
+        public string LastName { get; set; }
+        [StringLength(250)]
+        public string Adress { get; set; }
+        [StringLength(50)]
+        public string PostCode { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual ICollection<AppUser> Users { get; set; }
+
+    }
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
